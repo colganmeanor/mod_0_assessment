e493db6f1f501bcfa20b6c3c94b3442ccdbbed7d
Colgan Meanor <colganmeanor7@gmail.com>
2021-07-30 16:29:29 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var animal = "Bengal Tiger"
+var isCuddly = true
+animal = "Dragon"
+var num8 = 88
+var num3 = 33
+var quotient = (num3 / num8);
+var favoriteFilms = ["Star Wars", "Jurrasic Park", "Jaws"]
+favoriteFilms.shift();
+console.log(animal.length)
