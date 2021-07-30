b008484962fa2ea1231bfdb21c761a3469db79c4
Colgan Meanor <colganmeanor7@gmail.com>
2021-07-30 16:38:22 -0500

    Add house

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var HOUSE = {
+  color: "brown",
+  neighborhood: "Spring Hill",
+  numberOfRooms: 4,
+  numberOfBathrooms: 3.5,
+  isSafe: true,
+  isClean: false,
+  amenities: ["laundry", "fridge", "dishwasher"],
+  rooms: ["game room", "main bedroom", "kitchen", "living room"],
+}
