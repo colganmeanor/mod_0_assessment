d6624ad4478224c891e2c92b2655aa460ce79b6a
Colgan Meanor <colganmeanor7@gmail.com>
2021-07-30 16:34:57 -0500

    Add laptop

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var LAPTOP = {
+  brand: "dell",
+  screen: "4K",
+  screenInches: 15,
+  hardDriveSpace: 500,
+  isFast: true,
+  isSlow: false,
+  programs: ["Chrome", "Steam", "Discord"],
+  features: ["touch screen", "trackpad", "multiple USB ports"],
+}
